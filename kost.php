4604f500e660d1908c30d455734c394144d4832a
radityardiansyah11 <raditya.ardiansyah1234@gmail.com>
2024-09-22 23:28:06 +0700

    rev

--- kost.php
+++ kost.php
@@ -212,7 +212,6 @@ session_start();
         </div>
         <!-- Header End -->
 
-
         <!-- Search Start -->
         <div class="container-fluid bg-primary mb-5 wow fadeIn" data-wow-delay="0.1s" style="padding: 35px;">
             <div class="container">
