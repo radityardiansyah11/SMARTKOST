aa27e2210ccfdb0b7526361fdbe9b1c474ee96d3
radityardiansyah11 <raditya.ardiansyah1234@gmail.com>
2024-09-27 21:23:13 +0700

    rev add pk

--- add-pk.php
+++ add-pk.php
@@ -3,12 +3,12 @@ include 'config.php';
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $pkname = mysqli_real_escape_string($conn, $_POST['pkname']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $nomor_hp = mysqli_real_escape_string($conn, $_POST['nomor_hp']);
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    $sql = "INSERT INTO logsys_pk (username, email, nomor_hp, password) VALUES ('$username', '$email', '$nomor_hp', '$password')";
+    $sql = "INSERT INTO logsys_pk (pkname, email, nomor_hp, password) VALUES ('$pkname', '$email', '$nomor_hp', '$password')";
 
     if (mysqli_query($conn, $sql)) {
         $_SESSION['status'] = "added";
